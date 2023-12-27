8f54a12b2a94945367ea22146e6bbeb3390a44d1
med25 <m.chemmi25@gmail.com>
2023-12-27 13:09:24 -0500

    Commit : Implement TopBar,SearchBar and FloatingActionButton with Scaffold

--- app/src/main/java/com/example/nyn/ui/theme/Type.kt
+++ app/src/main/java/com/example/nyn/ui/theme/Type.kt
@@ -2,9 +2,25 @@ package com.example.nyn.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.nyn.R
+
+val Sen = FontFamily(
+    Font(R.font.sen_regular, FontWeight.Normal, FontStyle.Normal),
+
+    Font(R.font.sen_medium, FontWeight.Medium, FontStyle.Normal),
+
+    Font(R.font.sen_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+
+    Font(R.font.sen_bold, FontWeight.Bold, FontStyle.Normal),
+
+    Font(R.font.sen_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
