f49e6c72e70aa37e0d7e8b8799ef194456570bc5
med25 <m.chemmi25@gmail.com>
2024-01-06 15:14:55 -0500

    Commit : Adding animation to navigation from HomeSceen to AddNoteScreen

--- app/src/main/java/com/example/nyn/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/nyn/navigation/AppNavHost.kt
@@ -1,5 +1,7 @@
 package com.example.nyn.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,11 +26,26 @@ fun AppNavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(NavigationItem.HomeScreen.route) {
+        composable(route = NavigationItem.HomeScreen.route,) {
             val viewModel = hiltViewModel<HomeScreenViewModel>()
             HomeScreen(navHostController = navController,homeScreenViewModel = viewModel)
         }
-        composable(NavigationItem.AddNoteScreen.route) {
+        composable(route = NavigationItem.AddNoteScreen.route,
+
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(700)
+                )
+            }
+
+            ) {
             val viewModel = hiltViewModel<AddNoteViewModel>()
             AddNoteScreen(navHostController = navController, addNoteViewModel = viewModel)
         }
