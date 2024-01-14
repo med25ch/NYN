03401b82214c8cb76042a43a43c05d40df4ae56c
med25 <m.chemmi25@gmail.com>
2024-01-14 08:16:20 -0500

    Commit :  Save state for BottomSheet and Selected Category when configuration changes

--- app/src/main/java/com/example/nyn/screens/addnotescreen/AddNoteScreen.kt
+++ app/src/main/java/com/example/nyn/screens/addnotescreen/AddNoteScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -124,7 +125,7 @@ fun AddNoteScreen(modifier: Modifier = Modifier,
                   navHostController: NavHostController,
                   addNoteViewModel: AddNoteViewModel){
 
-    var showSheet by remember { mutableStateOf(false) }
+    var showSheet by rememberSaveable { mutableStateOf(false) }
 
     if (showSheet) {
         BottomSheet(modifier = modifier,
