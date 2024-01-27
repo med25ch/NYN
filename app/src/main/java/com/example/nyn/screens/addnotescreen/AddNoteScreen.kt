479dd727bdca046673080216c7625b842ebc06c2
med25ch <1804233@bdeb.qc.ca>
2024-01-27 10:09:09 -0500

    Commit : Using Lambdas to share composables between  screens

--- app/src/main/java/com/example/nyn/screens/addnotescreen/AddNoteScreen.kt
+++ app/src/main/java/com/example/nyn/screens/addnotescreen/AddNoteScreen.kt
@@ -1,143 +1,51 @@
 package com.example.nyn.screens.addnotescreen
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.PushPin
-import androidx.compose.material.icons.outlined.CreateNewFolder
-import androidx.compose.material.icons.outlined.PushPin
-import androidx.compose.material.icons.outlined.Save
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.nyn.data.models.note.Note
-import com.example.nyn.ui.theme.Sen
+import com.example.nyn.sharedui.SharedScaffold
 import kotlinx.coroutines.launch
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddNoteScreenScaffold(modifier: Modifier = Modifier,
                           addNoteViewModel: AddNoteViewModel,
                           navHostController: NavHostController,
                           onShowSheet: () -> Unit){
 
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     val coroutineScope = rememberCoroutineScope()
-    var isPinned by rememberSaveable { mutableStateOf(false) }
 
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.surface,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(
-                        "",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = { navHostController.popBackStack() }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { onShowSheet() }) {
-                        Icon(
-                            imageVector = Icons.Outlined.CreateNewFolder,
-                            contentDescription = "Localized description"
-                        )
-                    }
-
-                    IconButton(onClick = {
-                        isPinned = !isPinned
-                        addNoteViewModel.isPinnedNote(isPinned)
-                    }) {
-                        Icon(
-                            imageVector = if (isPinned)
-                                Icons.Filled.PushPin
-                            else
-                                Icons.Outlined.PushPin
-                                ,
-                            modifier = modifier.rotate(45f),
-                            contentDescription = "Localized description"
-                        )
-                    }
-
-                    IconButton(onClick = {
-                        coroutineScope.launch {
-                            addNoteViewModel.saveNoteToDB()
-                        }
-                        navHostController.popBackStack()
-                    }) {
-                        Icon(
-                            imageVector = Icons.Outlined.Save,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
-                scrollBehavior = scrollBehavior,
-            )
+    SharedScaffold(
+        navHostController = navHostController,
+        onShowSheet = onShowSheet,
+        modifier = modifier,
+        onClickPinnedNote =  { addNoteViewModel.isPinnedNote(it) },
+        onClickSaveNote = {
+            coroutineScope.launch {
+                addNoteViewModel.saveNoteToDB()
+            }
         },
-    ) {innerPadding ->
-        Column(
-            modifier = modifier
-                .padding(innerPadding),
-        ) {
-
-            // Note title Text
-            TitleNoteText(modifier,addNoteViewModel)
-
-            // Note Body
-            BodyNoteText(modifier,addNoteViewModel)
-        }
-    }
+        onTitleTextValueChange = { addNoteViewModel.updateNoteTitle(it) } ,
+        titleTextProvider = { addNoteViewModel.noteTitle.value },
+        onBodyTextValueChange = { addNoteViewModel.updateNoteBody(it)},
+        bodyTextProvider = { addNoteViewModel.noteBody.value }
+    )
 }
+
 
 @Composable
 fun AddNoteScreen(modifier: Modifier = Modifier,
                   navHostController: NavHostController,
                   addNoteViewModel: AddNoteViewModel,
                   argument : String?){
+
 
     val noteId = argument?.toIntOrNull()
 
@@ -156,60 +64,8 @@ fun AddNoteScreen(modifier: Modifier = Modifier,
     AddNoteScreenScaffold( modifier = modifier,
         addNoteViewModel = addNoteViewModel,
         navHostController = navHostController,
-        onShowSheet = { showSheet = true })
-}
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TitleNoteText(modifier: Modifier, addNoteViewModel: AddNoteViewModel) {
-
-    TextField(
-        value = addNoteViewModel.noteTitle.value ,
-        onValueChange = { addNoteViewModel.updateNoteTitle(it) },
-        label = { Text("Note title") },
-        maxLines = 3,
-        singleLine = false,
-        textStyle = TextStyle(
-            fontFamily = Sen,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.ExtraBold,
-        ),
-        modifier = Modifier
-            .fillMaxWidth(),
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = Color.Transparent,
-            unfocusedContainerColor = Color.Transparent,
-            disabledContainerColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
+        onShowSheet = { showSheet = true },
         )
-    )
 }
 
-@Composable
-fun BodyNoteText(modifier: Modifier = Modifier, addNoteViewModel: AddNoteViewModel) {
 
-    TextField(
-        value = addNoteViewModel.noteBody.value,
-        onValueChange = { addNoteViewModel.updateNoteBody(it) },
-        label = { Text("Note body") },
-        maxLines = 50,
-        textStyle = TextStyle(
-            fontFamily = Sen,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Normal,
-        ),
-        modifier = Modifier
-            .fillMaxSize(),
-        singleLine = false,
-        minLines = 15,
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = Color.Transparent,
-            unfocusedContainerColor = Color.Transparent,
-            disabledContainerColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
-        )
-    )
-}
---
+++ app/src/main/java/com/example/nyn/sharedui/SharedUiItems.kt
@@ -0,0 +1,195 @@
+package com.example.nyn.sharedui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.PushPin
+import androidx.compose.material.icons.outlined.CreateNewFolder
+import androidx.compose.material.icons.outlined.PushPin
+import androidx.compose.material.icons.outlined.Save
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.nyn.ui.theme.Sen
+
+@Composable
+fun TitleNoteText(modifier: Modifier,
+                  onTitleTextValueChange : (String) -> Unit,
+                  titleTextProvider : () -> String
+                  ) {
+
+    TextField(
+        value = titleTextProvider() ,
+        onValueChange = { onTitleTextValueChange(it) },
+        label = { Text("Note title") },
+        maxLines = 3,
+        singleLine = false,
+        textStyle = TextStyle(
+            fontFamily = Sen,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold,
+        ),
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+fun BodyNoteText(modifier: Modifier = Modifier,
+                 onBodyTextValueChange : (String) -> Unit,
+                 bodyTextProvider : () -> String) {
+
+    TextField(
+        value = bodyTextProvider(),
+        onValueChange = { onBodyTextValueChange(it) },
+        label = { Text("Note body") },
+        maxLines = 50,
+        textStyle = TextStyle(
+            fontFamily = Sen,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+        ),
+        modifier = modifier
+            .fillMaxSize(),
+        singleLine = false,
+        minLines = 15,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun SharedScaffold(
+    navHostController: NavHostController,
+    onShowSheet: () -> Unit,
+    modifier: Modifier,
+    onClickPinnedNote: (Boolean) -> Unit,
+    onClickSaveNote: () -> Unit,
+    onTitleTextValueChange : (String) -> Unit,
+    titleTextProvider : () -> String,
+    onBodyTextValueChange : (String) -> Unit,
+    bodyTextProvider : () -> String
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    var isPinned by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navHostController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { onShowSheet() }) {
+                        Icon(
+                            imageVector = Icons.Outlined.CreateNewFolder,
+                            contentDescription = "Localized description"
+                        )
+                    }
+
+                    IconButton(onClick = {
+                        isPinned = !isPinned
+                        onClickPinnedNote(isPinned)
+                    }) {
+                        Icon(
+                            imageVector = if (isPinned)
+                                Icons.Filled.PushPin
+                            else
+                                Icons.Outlined.PushPin,
+                            modifier = modifier.rotate(45f),
+                            contentDescription = "Localized description"
+                        )
+                    }
+
+                    IconButton(onClick = {
+                        onClickSaveNote()
+                        navHostController.popBackStack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Save,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding),
+        ) {
+
+            // Note title Text
+            TitleNoteText(
+                modifier,
+                onTitleTextValueChange = {onTitleTextValueChange(it)},
+                titleTextProvider = titleTextProvider
+            )
+
+            // Note Body
+            BodyNoteText(
+                modifier,
+                onBodyTextValueChange = { onBodyTextValueChange(it) },
+                bodyTextProvider =  bodyTextProvider
+            )
+        }
+    }
+}
